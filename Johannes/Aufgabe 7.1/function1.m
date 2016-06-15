87077a675111996433db41b1f26a6236dbb7c903
Johannes Kollek <johannes.kollek@udo.edu>
2016-06-15 23:07:53 +0200

    Aufgabe 7.1

---
+++ Johannes/Aufgabe 7.1/function1.m
@@ -0,0 +1,3 @@
+function [f] = function1 (x);
+f = (cos(2*x))^2-x^2;
+endfunction;
---
+++ Johannes/Aufgabe 7.1/myBisectTest.m
@@ -0,0 +1,6 @@
+function myBisectTest;
+f = @function1;
+[x,e,v] = mybisect(f,0,0.75);
+N = [1:1:length(e)];
+semilogy(N, e,'r-');
+endfunction;
